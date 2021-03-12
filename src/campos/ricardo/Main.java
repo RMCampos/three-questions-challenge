c0222d186b69303c4ab54894c7868524b83f01b1
Ricardo Campos <ricardo@kugel.com.br>
2021-03-12 14:14:27 -0300

    first commit

---
+++ src/campos/ricardo/Main.java
@@ -0,0 +1,8 @@
+package campos.ricardo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //
+    }
+}
---
+++ src/campos/ricardo/One.java
@@ -0,0 +1,47 @@
+package campos.ricardo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class One {
+
+    public static boolean validBracketSequence(String sequence) {
+        if (sequence == null || sequence.isEmpty()) {
+            return false;
+        }
+
+        final Map<Character,Character> charValidMap = new HashMap<>();
+        charValidMap.put('(', ')');
+        charValidMap.put('[', ']');
+        charValidMap.put('{', '}');
+
+        final List<Integer> positionsDone = new ArrayList<>();
+
+        final Map<Integer,Character> charsMap = new HashMap<>();
+        final char[] letters = sequence.toCharArray();
+        final int len = letters.length;
+
+        for (Integer key=0; key<len; key++) {
+            if (positionsDone.contains(key)) {
+                continue;
+            }
+
+            final Character current = letters[key];
+            final Character matching = letters[len-key-1];
+            final Character rightOne = charValidMap.get(current);
+
+            System.out.println(String.format("key[%d], current[%s], matching[%s], rightOne[%s]", key, current, matching, rightOne));
+
+            if (!matching.equals(rightOne)) {
+                return false;
+            }
+
+            positionsDone.add(key);
+            positionsDone.add(len-key-1);
+        }
+
+        return true;
+    }
+}
